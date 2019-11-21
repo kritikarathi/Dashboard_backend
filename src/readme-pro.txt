87207e51015c493a87628f0cc1cceed22f9e17b4
Naman Jain <naman.jain@apollomunichinsurance.com>
2019-11-21 13:56:36 +0530

    therapy add update api

---
+++ src/readme-pro.txt
@@ -0,0 +1,14 @@
+1. therapy 
+    backend api
+      add
+      delete
+      update
+
+    frontend
+
+2. indication
+
+3. product
+    multiple MOA
+    multiple indication
+    multiple company  
--- src/router/Therapy.js
+++ src/router/Therapy.js
@@ -24,4 +24,43 @@ router.get('/fetchALlTherapies',async (req,res)=>{
     })
 })
 
+router.post('/addNewTherapy', async (req, res) => {
+    try {
+        const therapy = new Therapy({
+            TherapyName: req.body.TherapyName,
+            Status: req.body.Status,
+            CreatedBy: req.body.CreatedBy
+        })
+    
+        await therapy.save()
+        res.status(200).json({
+            status: 'success',
+            therapy: therapy
+        })
+    } catch(err) {
+        res.status(400).json({
+            status: 'fail to add',
+            message: err
+        })
+    }
+})
+
+router.put('/updateTherapy/:id', async (req, res) => {
+    try {
+        const therapy = await Therapy.findByIdAndUpdate(req.params.id, {
+            new: true,
+            runValidators: true
+        })
+        res.status(201).json({
+            status: "successfully updated",
+            data: therapy
+        })
+    } catch(err) {
+        res.status(400).json({
+            status: 'fail to update',
+            message: err
+        })
+    }
+})
+
 module.exports = router
