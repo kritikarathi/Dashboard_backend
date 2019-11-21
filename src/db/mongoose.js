a0fc91410de7a99f3531bfd2516f1fed276a885f
Naman Jain <naman.jain@apollomunichinsurance.com>
2019-11-21 14:08:45 +0530

    change mongoo url

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose')
 // mongodb://localhost:27017/dashboard-api
 // mongodb+srv://admin_123:<password>@cluster0-v3b01.mongodb.net/test?retryWrites=true&w=majority
-mongoose.connect('mongodb://localhost:27017/dashboard-api', {
+mongoose.connect('mongodb+srv://admin_123:admin@123@cluster0-v3b01.mongodb.net/DashboardDb?retryWrites=true&w=majority', {
   useNewUrlParser: true,
   useCreateIndex: true,
   useUnifiedTopology: true
